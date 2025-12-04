1e1e7762d3bd32a14b2539a858f827e7bc378159
elia69 <elia.terragni@proton.me>
2025-12-04 16:20:32 +0100

    fix: Properly configure externals for mapbox-gl

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// Vite config for Vercel deployment
 export default defineConfig({
   plugins: [vue()],
-  base: './',
-  build: {
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  },
   server: {
     host: true
+  },
+  build: {
+    rollupOptions: {
+      external: [
+        'mapbox-gl',
+        'mapbox-gl/dist/mapbox-gl.css'
+      ],
+      output: {
+        globals: {
+          'mapbox-gl': 'mapboxgl',
+          'mapbox-gl/dist/mapbox-gl.css': 'mapboxGlCss'
+        }
+      }
+    },
+    commonjsOptions: {
+      include: [/node_modules/]
+    }
   }
 })
